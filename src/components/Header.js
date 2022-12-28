23e78b9330b0c1017925b0781e16275f4f2aad31
Nick Remley <remleynick@yahoo.com>
2022-12-28 14:19:27 -0700

    Company name next to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,49 @@
-import { useState } from 'react';
-import { 
-    Navbar, 
+import { useState } from "react";
+import {
+    Navbar,
     NavbarBrand,
     Collapse,
     NavbarToggler,
-    Nav, 
+    Nav,
     NavItem,
-} from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import BbhLogo from '../app/assests/bbhLogo2.png';
-
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import BbhLogo from "../app/assests/bbhLogo2.png";
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <Navbar dark sticky='top' expand='md'>
-            <NavbarBrand className='ms-5' href='/'>
-                <img src={BbhLogo} alt='Better Back Health Logo' className='float-start' />
+        <Navbar dark sticky="top" expand="md">
+            <NavbarBrand className="ms-5" href="/">
+                <img
+                    src={BbhLogo}
+                    alt="Better Back Health Logo"
+                    className="float-start"
+                />
+                <h3 className="mt-3">Better Back Health</h3>
             </NavbarBrand>
-            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
             <Collapse isOpen={menuOpen} navbar>
-                <Nav className='ms-auto' navbar>
+                <Nav className="ms-auto" navbar>
                     <NavItem>
-                        <NavLink className='nav-link' to='/'>
-                            <i className='fa fa-home fa-lg' /> Home
+                        <NavLink className="nav-link" to="/">
+                            <i className="fa fa-home fa-lg" /> Home
                         </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink className='nav-link' to='/about'>
-                            <i className='fa fa-list fa-lg' /> About
+                        <NavLink className="nav-link" to="/about">
+                            <i className="fa fa-list fa-lg" /> About
                         </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink className='nav-link' to='/exercise'>
-                            <i className='fa fa-heart fa-lg' /> Exercise
+                        <NavLink className="nav-link" to="/exercise">
+                            <i className="fa fa-heart fa-lg" /> Exercise
                         </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink className='nav-link' to='/contact'>
-                            <i className='fa fa-address-card fa-lg' /> Contact
+                        <NavLink className="nav-link" to="/contact">
+                            <i className="fa fa-address-card fa-lg" /> Contact
                         </NavLink>
                     </NavItem>
                 </Nav>
